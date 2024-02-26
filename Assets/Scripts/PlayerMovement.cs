62b004d3a41d90e2e035a782f0e31abbf3c386ca
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-02-27 01:23:46 +0700

    collision test
    add collision check dengan ground agar tidak infinite jump ketika hold space dan test oneway platform

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    public bool midAir;
+
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
@@ -14,8 +16,22 @@ public class PlayerMovement : MonoBehaviour
     private void Update() {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space)){
+        if (Input.GetKey(KeyCode.Space) && midAir == false){
             body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            midAir = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            midAir = true;
         }
     }
 }
