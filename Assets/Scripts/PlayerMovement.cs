38d9e9d740608d1d2eb9aeac4ff1eba8d04bf906
ArayaFeisa <arayafeisas@gmail.com>
2024-02-27 23:53:57 +0700

    adding raycast, fixing climb movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
-    private bool grounded;
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
     // public bool midAir;
     // public groundCheckTest check;
+    private Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCd;
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         if (horizontalInput > 0.01f){
             transform.localScale = Vector3.one;
@@ -25,26 +27,32 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded){
-            Jump();
+        if (wallJumpCd < 0.7f){
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (Input.GetKey(KeyCode.Space) && isGrounded()){
+                Jump();
+            }
+            if (onWall() && !isGrounded()){
+                body.gravityScale++;
+                body.velocity = Vector2.zero;
+            } else {
+                body.gravityScale = 25/10;
+            }
+            
+        } else {
+            wallJumpCd += Time.deltaTime;
         }
     }
-    
     private void Jump(){
         body.velocity = new Vector2(body.velocity.x, speed);
-        grounded = false;
     }
-
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+    private bool isGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
-
-    private void OnCollisionExit2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Ground")
-            grounded = false;
+    private bool onWall(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x,0), 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
-    // private bool isGrounded(){
-    //     return false;
-    // }
 }
