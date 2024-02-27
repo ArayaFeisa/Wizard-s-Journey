e58d71b57119f0f2d04caab041deca8542045cea
ArayaFeisa <arayafeisas@gmail.com>
2024-02-27 19:51:35 +0700

    movement fixing

--- Assets/Scripts/groundCheckTest.cs
+++ Assets/Scripts/groundCheckTest.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class groundCheckTest : MonoBehaviour
-{
-    public PlayerMovement check;
-    // Start is called before the first frame update
-    void Start()
-    {
+// public class groundCheckTest : MonoBehaviour
+// {
+//     public PlayerMovement check;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
         
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            check.midAir = false;
-        }
-    }
+//     }
+//     void OnCollisionEnter2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Ground"))
+//         {
+//             check.midAir = false;
+//         }
+//     }
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            check.midAir = true;
-        }
-    }
-}
+//     void OnCollisionExit2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Ground"))
+//         {
+//             check.midAir = true;
+//         }
+//     }
+// }
