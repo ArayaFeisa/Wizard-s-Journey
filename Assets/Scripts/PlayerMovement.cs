9a22989590d962b5e309ce9e787bf20c1c4c79d9
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-02-27 10:05:55 +0700

    groundCheck test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,19 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     [SerializeField] private float speed;
     public bool midAir;
-
+    public groundCheckTest check;
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
     }
-    
+
     private void Update() {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
         if (Input.GetKey(KeyCode.Space) && midAir == false){
             body.velocity = new Vector2(body.velocity.x, speed);
         }
+
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            midAir = false;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            midAir = true;
-        }
-    }
+    
 }
