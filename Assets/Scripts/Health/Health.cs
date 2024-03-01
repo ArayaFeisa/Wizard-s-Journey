7e73aee47cc093890e4eadf9c17a3159e0aeaeb4
ArayaFeisa <arayafeisas@gmail.com>
2024-03-01 22:41:40 +0700

    health system-1

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private bool dead;
+
+    private void Awake() {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(float _damage){
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0){
+            
+        }
+        else {
+            if (!dead){
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            TakeDamage(1);
+        }
+    }
+}
