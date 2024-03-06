7c03ca20673d82829834a0938444e8d819882212
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-03-06 16:28:35 +0700

    breakable cloud (lom animasinya)
    besok aja pas meet ajarin animasi mas wkwk

--- Assets/BreakableCloudScript.cs
+++ Assets/BreakableCloudScript.cs
@@ -7,25 +7,26 @@ public class BreakableCloudScript : MonoBehaviour
     // Start is called before the first frame update
     private float timer;
     private bool broken;
-    private Animator anim;
+    //private Animator anim;
     private BoxCollider2D collider;
+    private SpriteRenderer render;
     private bool action;
     void Start()
     {
         action = false;
         broken = false;
         timer = 0;
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
         collider = GetComponent<BoxCollider2D>();
+        render = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetTrigger("regenerating");
-        while (broken)
+        if (broken)
         {
-            if (timer < 3 && !action)
+            if (timer > 3 && !action)
             {
                 timer = 0;
                 StartCoroutine(regenerating());
@@ -53,9 +54,10 @@ public class BreakableCloudScript : MonoBehaviour
     private IEnumerator breaking()
     {
         action = true;
-        anim.SetTrigger("destroy");
+        //anim.SetTrigger("destroy");
         yield return new WaitForSeconds(1.2f);
         collider.enabled = false;
+        render.enabled = false;
         broken = true;
         action = false;
     }
@@ -63,9 +65,10 @@ public class BreakableCloudScript : MonoBehaviour
     private IEnumerator regenerating()
     {
         action = true;
-        anim.SetTrigger("regenerating");
+        //anim.SetTrigger("regenerating");
         yield return new WaitForSeconds(0.35f);
         collider.enabled = true;
+        render.enabled = true;
         broken = false;  
         action = false;
     }
