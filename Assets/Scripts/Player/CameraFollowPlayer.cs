4fcb68e6c7c1ae4068986db69be58933f5149e8b
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-03-09 05:16:40 +0700

    Revert "Revert "test""
    This reverts commit 7d4d54c4e42e14e9543837a56d367d7721ca9bc3.

--- Assets/Scripts/Player/CameraFollowPlayer.cs
+++ Assets/Scripts/Player/CameraFollowPlayer.cs
@@ -1,36 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using Cinemachine;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public CinemachineVirtualCamera vcam;
+    public Transform Player;
     void Start()
     {
-
+        
     }
 
-    private void Awake()
-    {
-        vcam = GetComponent<CinemachineVirtualCamera>();
-        StartCoroutine(zoomOut());
-    }
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    private IEnumerator zoomOut()
-    {
-        vcam.m_Lens.OrthographicSize = 0;
-        for (int i = 0; i < 66; i++)
-        {
-            vcam.m_Lens.OrthographicSize = vcam.m_Lens.OrthographicSize + 0.1f;
-            yield return new WaitForSeconds(0.015f);
-        }
+        transform.position = new Vector3(Player.position.x, Player.position.y, -10);
     }
 }
