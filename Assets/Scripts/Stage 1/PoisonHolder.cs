7060c10e975122d8173eab8cba5a7ec6b1fd6548
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-03-09 04:56:07 +0700

    test

---
+++ Assets/Scripts/Stage 1/PoisonHolder.cs
@@ -0,0 +1,12 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PoisonHolder : MonoBehaviour
+// {
+//     [SerializeField] private Transform enemy;
+
+//     private void Update() {
+//         transform.localScale = enemy.localScale;
+//     }
+// }
