689e59832575c99d5fa6853097d21b4d33985d65
DarveshAziz <144762772+DarveshAziz@users.noreply.github.com>
2024-03-09 05:16:53 +0700

    Revert "test"
    This reverts commit 7060c10e975122d8173eab8cba5a7ec6b1fd6548.

--- Assets/Scripts/Stage 2/EnemyFlying.cs
+++ Assets/Scripts/Stage 2/EnemyFlying.cs
@@ -7,24 +7,21 @@ public class EnemyFlying : MonoBehaviour
     [SerializeField] private float flySpeed;
     private GameObject player;
     public bool chase = false;
-    // private bool movingLeft;
-    [SerializeField] private Transform enemy;
-    [SerializeField] private Transform leftEdge;
-    [SerializeField] private Transform rightEdge;
     [SerializeField] private Transform startingPoint;
     private void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
     }
+
     private void Update() {
         if (player == null)
             return;
-
+            
         if (chase == true){
-            Chase();
-            Flip();
+            Chase();  
         } else {
             ReturnToStart();
         }
+        Flip();
     }
 
     private void Chase(){
@@ -39,20 +36,6 @@ public class EnemyFlying : MonoBehaviour
         }
     }
     private void ReturnToStart(){
-        if (transform.position.x > startingPoint.transform.position.x){
-            transform.localScale =  new Vector3(-3,3,3);
-        } else {
-            transform.localScale =  Vector3.one * 3f;
-        }
         transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, flySpeed * Time.deltaTime);
     }
-
-    // private void MoveIntoDirection(int _direction){
-    //     transform.position = new Vector3(transform.position.x + Time.deltaTime * flySpeed * _direction, 
-    //         transform.position.y, transform.position.z);
-    // }
-
-    // private void DirectionChange(){
-    //     movingLeft = !movingLeft;
-    // }
 }
