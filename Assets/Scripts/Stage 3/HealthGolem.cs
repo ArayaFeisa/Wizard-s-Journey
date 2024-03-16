7e2047d2f9499b66c34477242ef995473b55f16c
ArayaFeisa <128389876+ArayaFeisa@users.noreply.github.com>
2024-03-16 17:11:38 +0700

    added golem die anim

---
+++ Assets/Scripts/Stage 3/HealthGolem.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HealthGolem : MonoBehaviour
+{
+    [Header ("Health")]
+    public HealthBar healthBar;
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+    private bool dead;
+    private Animator anim;
+
+    [Header ("iframes")]
+    [SerializeField] private float immuneDuration;
+    [SerializeField] private int numberOfflashes;
+    private SpriteRenderer spriteRend;
+
+    [Header ("Components")]
+    [SerializeField] private Behaviour[] components;
+
+    private void Awake() {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+        healthBar.setMaxHealth(startingHealth);
+    }
+
+    public void takeDamage(float _damage){
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        healthBar.setHealth(currentHealth);
+        if (currentHealth > 0){
+            StartCoroutine(immune());
+        } else {
+            if (!dead){
+                // anim.SetBool("grounded", true);
+                Die();
+                dead = true;
+            }
+        }
+    }
+    private IEnumerator immune(){
+        Physics2D.IgnoreLayerCollision(7, 10, true);
+        for (int i = 0; i < numberOfflashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(immuneDuration / (numberOfflashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(immuneDuration / (numberOfflashes * 2));
+        }
+
+        Physics2D.IgnoreLayerCollision(7, 10, false);
+    }
+
+    private void AddHealth(float _value){
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    public void Respawn(){
+        dead = false;
+        AddHealth(startingHealth);
+        healthBar.setMaxHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("idle");
+        StartCoroutine(immune());
+
+        //aktifin semua komponen dikelasnya
+        foreach (Behaviour component in components) {
+            component.enabled = true;
+        }
+    }
+
+    public void Die(){
+        anim.SetTrigger("die");
+
+        //nonaktif semua komponen dikelasnya
+        foreach (Behaviour component in components) {
+            component.enabled = false;
+        }
+    }
+
+    private void deactivate(){
+        gameObject.SetActive(false);
+    }
+}
